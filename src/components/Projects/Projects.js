3c0b7816a4279913db600074fd9ceffac5b22d0f
Brandon Stiles <bstiles@merkleinc.com>
2020-10-09 08:32:11 -0400

    adds Projects content

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { map } from 'lodash';
+import { Card, Button, Icon, Image } from 'semantic-ui-react';
+import { projects } from '../../constants';
 
 import './Projects.scss';
 
+const mapProjects = () => {
+  return map(projects, (project, i) => {
+    return (
+      <Card key={`project-${i}`} className='projects-card'>
+        <Image src={project.image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{project.title}</Card.Header>
+          <Card.Meta>Created {project.created}</Card.Meta>
+          <Card.Description>
+            {project.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra className='projects-card-buttons'>
+          <Button
+            as={'a'}
+            href={project.url}
+            target='_blank'
+            color='green'
+          >
+            <Icon name='hand pointer outline' />
+              Open App
+          </Button>
+          <Button
+            as={'a'}
+            href={project.urlGithub}
+            target='_blank'
+            color='black'
+          >
+            <Icon name='github' />
+              GitHub
+          </Button>
+        </Card.Content>
+      </Card>
+    );
+  });
+};
+
 export const Projects = () => {
   return (
-    <div className='projects'>Projects</div>
-  )
-}
+    <div className='projects'>{mapProjects()}</div>
+  );
+};
---
+++ src/constants/projects.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    image: require('../assets/projects/wreck_finder.png'),
+    title: 'Wreck Finder',
+    description: 'Search for over 24,000 shipwrecks and obstructions recorded in United States waters. Wreck data is extracted from NOAA and imported into MongoDB. React-Redux frontend features an advanced search and user favorites. Backend is built with Node.js, Express, and MongoDB. Mapping via Leaflet.',
+    created: 2020,
+    url: 'https://wreck-finder.herokuapp.com/',
+    urlGithub: 'https://github.com/bstiles13/wreckfinder'
+  }
+];
