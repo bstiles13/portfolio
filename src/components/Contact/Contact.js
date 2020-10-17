aa8a728a805b0c399617c38b1d7354958fac5b2c
Brandon Stiles <bstiles@merkleinc.com>
2020-10-17 11:43:00 -0400

    responsive styling for contact info

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,36 +8,44 @@ export const Contact = () => {
     <div className='contact'>
       <h1>Connect with Me</h1>
       <div className='contact-row'>
-        <Icon className='contact-row-icon' name='send'></Icon>
-        <div className='contact-row-label'>Email</div>
-        <div className='contact-row-divider'>{`//`}</div>
+        <div className='contact-row-label'>
+          <Icon className='contact-row-icon' name='send'></Icon>
+          <span>Email</span>
+          <span className='contact-row-divider'>{`//`}</span>
+        </div>
         <div className='contact-row-link'>
           <a href='mailto:bstiles13@gmail.com'>bstiles13@gmail.com</a>
         </div>
       </div>
 
       <div className='contact-row'>
-        <Icon className='contact-row-icon' name='github' />
-        <div className='contact-row-label'>GitHub</div>
-        <div className='contact-row-divider'>{`//`}</div>
+        <div className='contact-row-label'>
+          <Icon className='contact-row-icon' name='github' />
+          <span>GitHub</span>
+          <span className='contact-row-divider'>{`//`}</span>
+        </div>
         <div className='contact-row-link'>
           <a href='https://github.com/bstiles13' rel='noopener noreferrer' target='_blank'>https://github.com/bstiles13</a>
         </div>
       </div>
 
       <div className='contact-row'>
-        <Icon className='contact-row-icon' name='linkedin' />
-        <div className='contact-row-label'>LinkedIn</div>
-        <div className='contact-row-divider'>{`//`}</div>
+        <div className='contact-row-label'>
+          <Icon className='contact-row-icon' name='linkedin' />
+          <span>LinkedIn</span>
+          <span className='contact-row-divider'>{`//`}</span>
+        </div>
         <div className='contact-row-link'>
           <a href='https://www.linkedin.com/in/brandon-stiles/' rel='noopener noreferrer' target='_blank'>https://www.linkedin.com/in/brandon-stiles/</a>
         </div>
       </div>
 
       <div className='contact-row'>
-        <Icon className='contact-row-icon' name='stack overflow' />
-        <div className='contact-row-label'>StackOverflow</div>
-        <div className='contact-row-divider'>{`//`}</div>
+        <div className='contact-row-label'>
+          <Icon className='contact-row-icon' name='stack overflow' />
+          <span>StackOverflow</span>
+          <span className='contact-row-divider'>{`//`}</span>
+        </div>
         <div className='contact-row-link'>
           <a href='https://stackoverflow.com/users/7659370/brandon-stiles' rel='noopener noreferrer' target='_blank'>https://stackoverflow.com/users/7659370/brandon-stiles</a>
         </div>
